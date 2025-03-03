Lots of changes: fix main bug (sign error in variational log likelihood), but still lots of open-ended questions and design choices (hessian, synthetic labels, etc.). Also add make_debug target (super useful) and some more unit tests.

src/ggn.py
@@ -23,29 +23,42 @@ def second_derivative_outputs_nll(params, xi, yi, apply_fn):
     exp_neg_logvar = jnp.exp(-logvar)
     # Second derivative components:
     d2L_dmu2 = exp_neg_logvar
-    d2L_dmu_dlogvar = (yi - mu) * exp_neg_logvar
+    # d2L_dmu_dlogvar = (yi - mu) * exp_neg_logvar
     d2L_dlogvar2 = 0.5 * (yi - mu)**2 * exp_neg_logvar
-    H = jnp.array([[d2L_dmu2,      d2L_dmu_dlogvar],
-                   [d2L_dmu_dlogvar, d2L_dlogvar2]])
+    # H = jnp.array([[d2L_dmu2,      d2L_dmu_dlogvar],
+    #                [d2L_dmu_dlogvar, d2L_dlogvar2]])
+    H = jnp.array([[d2L_dmu2,      jnp.array([0.])],
+                   [jnp.array([0.]), d2L_dlogvar2]]) # todo: major fix needed here! This is a hack
+    # H = jnp.eye(2)
     return H
 
 
-def compute_ggn(state, x, y, full_set_size=None):
+# todo more (memory-)efficient implementation of all of this? :D
+def compute_ggn(state, x, y=None, full_set_size=None):
     flat_params, unravel_fn = jax.flatten_util.ravel_pytree(state.params)
-
-    def model_outputs(flatp, xi):
+    def model_fun(flatp, xi):
         p_unr = unravel_fn(flatp)
         return state.apply_fn(p_unr, xi)
     
-    # todo more (memory-)efficient implementation of all of this? :D
-    jac_tuple = jax.vmap(lambda xi: jax.jacobian(lambda p: model_outputs(p, xi))(flat_params))(x)
+    jac_tuple = jax.vmap(
+        lambda xi: jax.jacobian(
+            lambda p: model_fun(p, xi)
+            )(flat_params)
+        )(x)
     J = jnp.stack(jac_tuple, axis=1).squeeze()
-    H = jax.vmap(lambda xi, yi: second_derivative_outputs_nll(unravel_fn(flat_params), xi, yi, state.apply_fn))(x, y).squeeze()
-    
-    def per_sample_ggn(Ji, Hi):
-        return Ji.T @ Hi @ Ji
-
-    GGN = jax.vmap(per_sample_ggn)(J, H).sum(axis=0)
+    if y is not None:
+        H = jax.vmap(lambda xi, yi: second_derivative_outputs_nll(unravel_fn(flat_params), 
+                                                                  xi, 
+                                                                  yi, 
+                                                                  state.apply_fn))(x, y).squeeze()
+        def per_sample_ggn(Ji, Hi):
+            return Ji.T @ Hi @ Ji
+        GGN = jax.vmap(per_sample_ggn)(J, H).sum(axis=0)
+    else:
+        def per_sample_ggn(Ji):
+            return Ji.T @ Ji
+        GGN = jax.vmap(per_sample_ggn)(J).sum(axis=0)
+        
     
     # ! Recalibration term (if necessary) 
     # todo maybe make learnable?

src/lla.py
@@ -2,20 +2,32 @@ import jax
 import jax.numpy as jnp
 import tensorflow_probability.substrates.jax as tfp
 
-from ggn import ensure_symmetry, compute_ggn
+from src.ggn import ensure_symmetry, compute_ggn
+
+def compute_curvature_approx(map_state, dataset, prior_std, full_set_size=None, return_Hinv=True):
+    x,y = dataset
+    GGN, flat_params_map, unravel_fn = compute_ggn(map_state, x, y, full_set_size=full_set_size) 
+    prior_precision = 1.0 / (prior_std**2)
+    GGN += prior_precision*jnp.eye(GGN.shape[0])
+    GGN = ensure_symmetry(GGN) # ! expensive, might not be needed
+    if return_Hinv: return GGN, flat_params_map, unravel_fn
+    else: return jnp.linalg.inv(GGN), flat_params_map, unravel_fn
 
 
 def posterior_lla(map_state, prior_std, x, y=None, full_set_size=None, return_unravel_fn=False):
     """
     Posterior parameter distribution.
     """
+    # todo handle this correctly!
     if y is None:
         y,logvar = jax.lax.stop_gradient(map_state.apply_fn(map_state.params, x)) # ! use real labels y if provided or the inducing point function values
-    GGN, flat_params_map, unravel_fn = compute_ggn(map_state, x, y, full_set_size=full_set_size) 
-    prior_precision = 1.0 / (prior_std**2)
-    GGN += prior_precision*jnp.eye(GGN.shape[0])
-    GGN = ensure_symmetry(GGN)
-    H_approx = jnp.linalg.inv(GGN)
+    
+    H_approx, flat_params_map, unravel_fn = compute_curvature_approx(map_state, 
+                                                                     (x,y), 
+                                                                     prior_std,
+                                                                     full_set_size=full_set_size, 
+                                                                     return_Hinv=False)
+
     posterior_dist = tfp.distributions.MultivariateNormalFullCovariance(
             loc=flat_params_map.astype(jnp.float64), # todo: cast to f64 or other to f32?
             covariance_matrix=H_approx
@@ -25,17 +37,19 @@ def posterior_lla(map_state, prior_std, x, y=None, full_set_size=None, return_un
     return posterior_dist
 
 
-def predict_lla(map_state, xnew, xdata, ydata=None, prior_std=1.0, full_set_size=None):
+def predict_lla(map_state, xnew, x, y=None, prior_std=1.0, full_set_size=None):
     """
     Posterior predictive distribution.
     """
-    if ydata is None:
-        ydata,logvar = jax.lax.stop_gradient(map_state.apply_fn(map_state.params, xdata)) # ! use real labels y if provided or the inducing point function values
-    GGN, flat_params_map, unravel_fn = compute_ggn(map_state, xdata, ydata, full_set_size=full_set_size) 
-    prior_precision = 1.0 / (prior_std**2)
-    GGN += prior_precision*jnp.eye(GGN.shape[0])
-    GGN = ensure_symmetry(GGN)
-    H_approx = jnp.linalg.inv(GGN)
+    # todo handle this correctly!
+    if y is None:
+        y,logvar = jax.lax.stop_gradient(map_state.apply_fn(map_state.params, x)) # ! use real labels y if provided or the inducing point function values
+    
+    H_approx, flat_params_map, unravel_fn = compute_curvature_approx(map_state, 
+                                                                     (x,y), 
+                                                                     prior_std,
+                                                                     full_set_size=full_set_size, 
+                                                                     return_Hinv=False)
     
     @jax.jit
     def flat_apply_fn(flat_p, inputs):

src/main.py
@@ -165,12 +165,15 @@ def main():
     # =========== PART B: Inducing Points ===========
     induc_ckpt_name = f"ind_{args.dataset}"
     rng_inducing = jax.random.PRNGKey(seed_inducing)
-    xinit = xtrain
+    # xinit = xtrain
     # xinit = jnp.linspace(xtrain.min(), xtrain.max(), m_induc)[:,None]
+    _, test_loader = get_dataloaders(train_dataset, test_dataset, min(m_induc,len(test_dataset)))
+    xinit = next(iter(test_loader))[0]
 
     if args.mode in ["train_inducing", "full_pipeline"]:
         xoptimizer = optax.adam(lr_induc)
-        full_dataset = (xtrain, ytrain)
+        # print("WARNING! Training inducing with full dataset!")
+        # train_loader, _ = get_dataloaders(train_dataset, test_dataset, len(train_dataset))
         
         xinduc = train_inducing_points(
             map_model_state, 

tests/test_probs.py
@@ -5,21 +5,6 @@ import jax.numpy as jnp
 from src.train_map import nl_posterior_fun, nl_likelihood_fun, nl_prior_fun
 from fixtures import regression_1d_data, small_model_state
 
-# todo 1. Simple synthetic distributions
-    # todo 1a: small synthetic model (known params)
-    # todo 1b: small synthetic dataset from known model
-    # todo 1c: compute log likelihood with nl_likelihood_fun
-    # todo 1d: compare to manually computed log likelihood
-
-# todo 2. Toy posterior setup
-    # todo 2a: compute log prior manually
-    # todo 2b: combine with log likelihood
-    # todo 2c: check nl_posterior_fun = nl_likelihood + nl_prior
-
-
-# todo 3. Verify learned variance term!
-    # todo 3a: how??
-
 
 # 1. Testing the negative log likelihood (nl_likelihood_fun)
 def test_nl_likelihood_fun(regression_1d_data, small_model_state):
@@ -33,15 +18,16 @@ def test_nl_likelihood_fun(regression_1d_data, small_model_state):
     # Set known parameters
     state.params["W"] = 2.0
     state.params["b"] = 0.0
-    state.params["logvar"] = 0.0
+    state.params["logvar"] = 2.3
 
     nll_fun_val = nl_likelihood_fun(state.apply_fn, state.params, regression_1d_data)
 
     # Manually compute the nll
     manual_nll = 0.0
+    sigma2 = jnp.exp(2.3)  # variance corresponding to logvar=2.3
     for xi, yi in zip(X, y):
         mu = 2.0 * xi + 0.0
-        manual_nll += 0.5 * (jnp.log(2 * jnp.pi) + 0.0 + ((yi - mu)**2) / 1.0)
+        manual_nll += 0.5 * (jnp.log(2 * jnp.pi) + 2.3 + ((yi - mu)**2) / sigma2)
 
     np.testing.assert_allclose(nll_fun_val, manual_nll, rtol=1e-4, atol=1e-6)
 
@@ -87,6 +73,7 @@ def test_nl_posterior_fun(regression_1d_data, small_model_state):
     
     np.testing.assert_allclose(posterior_val, nll_val + prior_val, rtol=1e-4, atol=1e-6)
 
+
 # 4. Verifying the learned variance term's effect.
 def test_learned_variance_effect(regression_1d_data, small_model_state):
     """
@@ -102,9 +89,9 @@ def test_learned_variance_effect(regression_1d_data, small_model_state):
     state.params["b"] = 0.0
 
     # Compute nll for two different logvar values.
-    state.params["logvar"] = -1.0  # variance = exp(-1) ~ 0.3679
+    state.params["logvar"] = -1.0  # variance = exp(-1) ~ 0.37
     nll_low = nl_likelihood_fun(state.apply_fn, state.params, regression_1d_data)
-    state.params["logvar"] = 1.0   # variance = exp(1) ~ 2.7183
+    state.params["logvar"] = 1.0   # variance = exp(1) ~ 2.72
     nll_high = nl_likelihood_fun(state.apply_fn, state.params, regression_1d_data)
 
     # They should be different. (You could further manually compute the expected change,