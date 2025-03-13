22d73127a2c359e06e532a20fe9b8ed65779c5b1
Niels Raunkjær Holm <nielsraunkjaer@gmail.com>
2025-03-13 18:49:40 +0100

    Implement dense NA-Hutch++ trace estimator, move all trace estimators into stochtrace.py

--- src/ggn.py
+++ src/ggn.py
@@ -1,73 +1,8 @@
 import pdb
 import jax
 import jax.numpy as jnp
-from jax import lax
-import tensorflow_probability.substrates.jax.distributions as tfd
 
 from src.utils import flatten_nn_params
-
-
-def stochastic_trace_estimator_full(X, seed, num_samples=1_000, symmetrize=True):
-    """
-    Uses Girard-Hutchinson estimator on fully instantiated matrices.
-    # ! quite unstable! Bug?
-    """
-    X = ensure_symmetry(X) if symmetrize else X
-    def sample_eps(X, seed, num_samples): 
-        return jax.random.rademacher(key=seed, shape=(num_samples, X.shape[0]))
-        # return jax.random.normal(key=seed, shape=(num_samples, X.shape[0]))
-    def single_estimate(X, eps):
-        y = jnp.matmul(X, eps)
-        return jnp.dot(eps, y)
-    eps = sample_eps(X, seed=seed, num_samples=num_samples)
-    estimates = jax.vmap(single_estimate, in_axes=(None,0))(X,eps)
-    return estimates.mean()
-
-
-def stochastic_trace_estimator_full_2(X, seed, num_samples=10):
-    """
-    Uses Hutch++ on fully instantiated matrices.
-    """
-    num_samples = num_samples if num_samples % 2 == 0 else num_samples + 1 # ensure evenness.
-    def sample_eps(): 
-        """Sample isotropic random vectors, either from N(0,I) or with Rademacher dist. (unif{-1,+1} indices)"""
-        # return jax.random.rademacher(key=seed, shape=(num_samples, X.shape[0]))
-        return jax.random.normal(key=seed, shape=(num_samples * 2, X.shape[0]))
-    S,G = jnp.split(sample_eps(), 2, axis=0)
-    Q,R = jnp.linalg.qr(X @ S.T)
-    Q_term = (jnp.eye(Q.shape[0]) - Q@Q.T)
-    estimates = jnp.trace(Q.T@X@Q) + (1/num_samples) * jnp.trace(G@Q_term@X@Q_term@G.T)
-    
-    return estimates.mean()
-
-
-def stochastic_trace_estimator_jvp(Xfun, D, seed, num_samples=10):
-    """
-    Uses Hutch++ with jvp.
-    Xfun: oracle computing v |-> X@v, where X: square matrix
-    D: int, dim(X)
-    """
-    num_samples = num_samples if num_samples % 2 == 0 else num_samples + 1 # ensure evenness.
-    def sample_eps(): 
-        """Sample isotropic random vectors, either from N(0,I) or Rademacher dist. (i.e. unif{-1,+1} indices)"""
-        # return jax.random.rademacher(key=seed, shape=(num_samples, D))
-        return jax.random.normal(key=seed, shape=(num_samples * 2, D))
-    S,G = jnp.split(sample_eps(), 2, axis=0)
-    Q,R = jnp.linalg.qr(Xfun(S.T))
-    orthproj = (jnp.eye(Q.shape[0]) - Q@Q.T) # symmetric
-    
-    def quad_term(M):
-        """
-        Compute M^T X M
-        as M.T@X@M = M.T@(X@M)
-        """
-        Y = jax.vmap(Xfun, in_axes=1, out_axes=1)(M)
-        return M.T @ Y
-    
-    estimates = jnp.trace(quad_term(Q)) + (1/num_samples) * jnp.trace(quad_term(orthproj@G.T))
-    
-    return estimates.mean()
-
 
 
 def compute_ggn_vp(state, Z, w, V, model_type, full_set_size=None):
---
+++ src/stochtrace.py
@@ -0,0 +1,78 @@
+import jax
+import jax.numpy as jnp
+
+
+def stochastic_trace_estimator_full(X, seed, num_samples=1_000):
+    """
+    Uses Girard-Hutchinson estimator on fully instantiated matrices.
+    # ! quite unstable! Bug?
+    """
+    def sample_eps(X, seed, num_samples): 
+        return jax.random.rademacher(key=seed, shape=(num_samples, X.shape[0]))
+        # return jax.random.normal(key=seed, shape=(num_samples, X.shape[0]))
+    def single_estimate(X, eps):
+        y = jnp.matmul(X, eps)
+        return jnp.dot(eps, y)
+    eps = sample_eps(X, seed=seed, num_samples=num_samples)
+    
+    return jax.vmap(single_estimate, in_axes=(None,0))(X,eps)
+
+
+def stochastic_trace_estimator_full_2(X, seed, num_samples=10):
+    """
+    Uses Hutch++ with fully instantiated matrices.
+    """
+    # ? Sample isotropic random vectors, either from N(0,I) or with Rademacher dist. (unif{-1,+1} indices)
+    # eps = jax.random.rademacher(key=seed, shape=(num_samples * 2, X.shape[0]))
+    eps = jax.random.normal(key=seed, shape=(num_samples * 2, X.shape[0])) 
+    S,G = jnp.split(eps, 2, axis=0)
+    
+    Q,R = jnp.linalg.qr(X @ S.T)
+    orthproj = (jnp.eye(Q.shape[0]) - Q@Q.T) # symmetric
+    
+    return jnp.trace(Q.T@X@Q) + (1/num_samples) * jnp.trace(G@orthproj@X@orthproj@G.T)
+
+
+def stochastic_trace_estimator_full_3(X, seed, num_samples=10):
+    """
+    Uses NA-Hutch++ with fully instantiated matrices.
+    """
+    c1,c2,c3 = .25,.5,.25 # good values, given in Hutch++ paper.
+    # ? Sample isotropic random vectors, either from N(0,I) or with Rademacher dist. (unif{-1,+1} indices)
+    eps = jax.random.rademacher(key=seed, shape=(num_samples * 4, X.shape[0]))
+    # eps = jax.random.normal(key=seed, shape=(num_samples * 4, X.shape[0])) 
+    S,R,G = jnp.split(eps, [num_samples, num_samples*3], axis=0) # split into [1/4, 2/4, 1/4]
+    W = X @ S.T
+    Z = X @ R.T
+    
+    return jnp.trace(jnp.linalg.pinv(S@Z) @ (W.T@Z)) + (1/(c3*4*num_samples)) * (jnp.trace(G@X@G.T) - jnp.trace(G@Z@jnp.linalg.pinv(S@Z)@W.T@G.T))
+
+
+def stochastic_trace_estimator_jvp(Xfun, D, seed, num_samples=10):
+    """
+    Uses Hutch++ with linear operator oracle function.
+    - `Xfun`: oracle computing v -> X@v, where X: square matrix
+    - `D`: int, dim(X)
+    """
+    
+    # ? Sample isotropic random vectors, either from N(0,I) or Rademacher dist. (i.e. unif{-1,+1} indices)
+    # eps = jax.random.rademacher(key=seed, shape=(num_samples * 2, D))
+    eps = jax.random.normal(key=seed, shape=(num_samples * 2, D))
+    S,G = jnp.split(eps, 2, axis=0)
+    
+    Q,R = jnp.linalg.qr(Xfun(S.T))
+    orthproj = (jnp.eye(Q.shape[0]) - Q@Q.T) # symmetric
+    
+    def quad_term(M):
+        """
+        Compute M^T X M
+        as M.T@X@M = M.T@(X@M)
+        """
+        Y = jax.vmap(Xfun, in_axes=1, out_axes=1)(M)
+        return M.T @ Y
+    
+    estimates = jnp.trace(quad_term(Q)) + (1/num_samples) * jnp.trace(quad_term(orthproj@G.T))
+    
+    return estimates.mean()
+
+# todo could also implement XTrace? Seems to not be a better choice for our case, so defer...
